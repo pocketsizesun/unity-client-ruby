3aee0b3eb4fd3eabc43f5a58e67f5d8d0a458c76
Julien D <julien@pocketsizesun.com>
2021-02-22 09:22:20 +0100

    add serialize methods

--- lib/unity/client/result.rb
+++ lib/unity/client/result.rb
@@ -21,6 +21,18 @@ module Unity
       def respond_to_missing?(method_name, include_private = true)
         @data.key?(method_name.name) || super
       end
+
+      def as_json
+        @data.as_json
+      end
+
+      def to_json(*args)
+        @data.to_json(*args)
+      end
+
+      def to_h
+        @data
+      end
     end
   end
 end
--- lib/unity/client/version.rb
+++ lib/unity/client/version.rb
@@ -1,5 +1,5 @@
 module Unity
   class Client
-    VERSION = '1.1.0'
+    VERSION = '1.1.1'
   end
 end
