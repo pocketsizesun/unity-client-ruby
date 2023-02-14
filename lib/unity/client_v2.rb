6f9d3ff565636debc457a88b62b938bf8d540529
Julien D <julien@pocketsizesun.com>
2023-02-14 11:39:04 +0100

    Update client_v2.rb

--- lib/unity/client_v2.rb
+++ lib/unity/client_v2.rb
@@ -15,31 +15,28 @@ module Unity
       @http_connection_timeout = options.fetch(:http_connection_timeout, 2)
       @http_write_timeout = options.fetch(:http_write_timeout, 3)
       @http_read_timeout = options.fetch(:http_read_timeout, 10)
+      @http_client = HTTP.persistent(@endpoint).timeout(
+        connect: @http_connection_timeout,
+        write: @http_write_timeout,
+        read: @http_read_timeout
+      )
     end
 
     def get(path, parameters = {})
-      resp = http_client.get(path, params: parameters).flush
+      resp = @http_client.get(path, params: parameters).flush
       raise_error(resp) if resp.code >= 400
 
       Unity::Client::Result.from_response(resp)
     end
 
     def post(path, parameters = {})
-      resp = http_client.post(path, json: parameters).flush
+      resp = @http_client.post(path, json: parameters).flush
       raise_error(resp) if resp.code >= 400
 
       Unity::Client::Result.from_response(resp)
     end
 
     private
-
-    def http_client
-      @http_client ||= HTTP.persistent(@endpoint).timeout(
-        connect: @http_connection_timeout,
-        write: @http_write_timeout,
-        read: @http_read_timeout
-      )
-    end
 
     def raise_error(resp)
       raise Unity::Client::ResponseError.from(resp)
